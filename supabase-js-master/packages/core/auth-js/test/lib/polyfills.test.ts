248221786a7737bb2f485fbc773355c047d78dee
gopalsdiary <gopalsdiary@gmail.com>
2026-01-26 22:05:37 +0600

    sa

--- supabase-js-master/packages/core/auth-js/test/lib/polyfills.test.ts
+++
@@ -1,7 +0,0 @@
-import { polyfillGlobalThis } from '../../src/lib/polyfills'
-
-describe('polyfillGlobalThis', () => {
-  it('should be defined as a function', () => {
-    expect(typeof polyfillGlobalThis).toBe('function')
-  })
-})
